ee2f7579430b428e819c5d3f31c015fb552c6e79
jmlaranjeira <juan.laranjeira@dehn.de>
2022-11-04 16:14:25 +0100

    first commit

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,78 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { NotFoundException } from '@nestjs/common';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
+
+  beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'asdf@asdf.com',
+          password: 'asdf',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([
+          { id: 1, email: 'asdf@asdf.com', password: 'asdf' } as User,
+        ]);
+      },
+      // remove: (id: number) => {},
+      // update: (id: number, attrs: Partial<User>) => {}
+    };
+    fakeAuthService = {
+      // signup: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      }
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('findAllUsers returns a list of users with the given email', async() => {
+    const users = await controller.findAllUsers('asdf@asdf.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('signin updates session object and returns user', async() => {
+    const session = { userId: -10 };
+    const user = await controller.signin({ email: 'asdf@asdf.com', password: 'asdf'},
+    session);
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  })
+
+});
